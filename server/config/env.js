ad9a6f5b61206fa7aca7159c422380ee87ce020e
cerramaximiliano <cerramaximiliano@gmail.com>
2022-10-28 16:26:03 -0300

    v1.00

--- server/config/env.js
+++ server/config/env.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 module.exports = () => {
     const secretManager = new AWS.SecretsManager({ region: 'sa-east-1'});
     return new Promise ((resolve, reject) => {
-        secretManager.getSecretValue({ SecretId: 'arn:aws:secretsmanager:sa-east-1:244807945617:secret:env-8tdon8' }, (err, data) => {
+        secretManager.getSecretValue({ SecretId: '***********' }, (err, data) => {
             if(err) {
                 reject (err)
             }else {
